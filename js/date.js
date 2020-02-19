679af963ee91ca687698582c9624499bcb28663a
Timoking <13466083337@163.com>
2020-02-19 17:57:07 +0800

    review Date

---
+++ js/date.js
@@ -0,0 +1,9 @@
+var d = new Date();
+console.log(d);
+
+console.log(d.toUTCString());
+console.log(d.toDateString());
+console.log(d.getDate());
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getTime());
