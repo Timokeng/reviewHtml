b0fbdaa0808b5a072dc3f8114abc4b2f37232141
Timoking <13466083337@163.com>
2021-04-20 13:53:13 +0800

    review typeof

---
+++ js/typeof.js
@@ -0,0 +1,15 @@
+var a = 1,
+b = "1",
+c = true,
+d = null,
+e = undefined,
+f = [],
+g = function(){};
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(g));
